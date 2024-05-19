d40df31a7439159c75f65201bcac27472d3c7235
Steve Potts <coolpot_1@yahoo.com>
2024-05-19 13:42:53 +0100

    added basic layout

---
+++ admin/src/components/AppLayout.vue
@@ -0,0 +1,28 @@
+
+
+<template>
+    <div class="flex min-h-full bg-gray-100">
+        <Sidebar></Sidebar>
+        <div class="flex-1">
+            <TopHeader/>
+            <main class="p-6">
+                <div class="p-4 rounded bg-white">
+                    <router-view></router-view>
+                </div>
+            </main>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import Sidebar from "./Sidebar.vue";
+    import TopHeader from "./TopHeader.vue";
+
+    const { title } = defineProps({
+        title: String
+    })
+</script>
+
+<style scoped>
+
+</style>
---
+++ admin/src/components/Sidebar.vue
@@ -0,0 +1,63 @@
+
+
+<template>
+
+    <div class="w-[200px] bg-indigo-700 text-gray-300 py-4 px-2">
+        <router-link
+            :to="{name: 'app.dashboard'}"
+            class="flex items-center transition-colors
+                        py-1 px-2 rounded hover:bg-black/30 mb-2"
+        >
+            <span class="mr-2 text-gray-300">
+                <HomeIcon class="w-5"/>
+            </span>
+            <span class="text-sm font-light">
+                Dashboard
+            </span>
+        </router-link>
+        <router-link
+            :to="{name: 'app.dashboard'}"
+            class="flex items-center transition-colors
+                        py-1 px-2 rounded hover:bg-black/30 mb-2"
+        >
+            <span class="mr-2 text-gray-300">
+                <ViewListIcon class="w-5"/>
+            </span>
+            <span class="text-sm font-light">
+                Products
+            </span>
+        </router-link>
+        <router-link
+            :to="{name: 'app.dashboard'}"
+            class="flex items-center transition-colors
+                        py-1 px-2 rounded hover:bg-black/30 mb-2"
+        >
+            <span class="mr-2 text-gray-300">
+                <UsersIcon class="w-5"/>
+            </span>
+            <span class="text-sm font-light">
+                Users
+            </span>
+        </router-link>
+        <router-link
+            :to="{name: 'app.dashboard'}"
+            class="flex items-center transition-colors
+                        py-1 px-2 rounded hover:bg-black/30"
+        >
+            <span class="mr-2 text-gray-300">
+                <ChartBarIcon class="w-5"/>
+            </span>
+            <span class="text-sm font-light">
+                Reports
+            </span>
+        </router-link>
+    </div>
+</template>
+
+<script setup>
+    import { HomeIcon, UsersIcon, ViewListIcon, ChartBarIcon} from "@heroicons/vue/outline";
+</script>
+
+<style scoped>
+
+</style>
